15c15a2bfb60eaca34284efc67f2fb0012292db6
Reuven Koblick <reuven@g.harvard.edu>
2016-03-04 10:00:52 -0500

    Empty project

---
+++ extensions.py
@@ -0,0 +1,11 @@
+try:
+    # only works in debug mode
+    from flask_debugtoolbar import DebugToolbarExtension
+
+    toolbar = DebugToolbarExtension()
+except ImportError:
+    print('debugtoolbar extension not available.')
+from flask.ext.sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
+
+
