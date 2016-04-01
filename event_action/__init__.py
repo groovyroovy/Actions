b2ed9bee16ad4b7cafa1bf50a843825b222303fc
Reuven Koblick <reuven@g.harvard.edu>
2016-04-01 14:47:38 -0400

    More debugging: mail issues. Need to update verify lookup after catching an action. Put actions in event_action directory. Project will be eventually be called Flask-EventAction.

---
+++ event_action/__init__.py
@@ -0,0 +1,10 @@
+from actions import event_registry
+from actions import act_manager
+from actions import db_event_pitcher
+from actions import db_event
+from actions import init_app
+from actions import db_rollback
+
+from actions import ActionManager
+from actions import Action
+from actions import EmailAction
--- iggy_actions.py
+++ iggy_actions.py
@@ -1,4 +1,4 @@
-from actions import ActionManager, EmailAction
+from event_action import ActionManager, EmailAction
 from flask import current_app
 from database import db_session
 from models import User
@@ -6,14 +6,15 @@ from models import User
 act_mgr = ActionManager(current_app, db_session)
 
 msg = {
-    'recipients': ('Reuven Koblick', 'reuven@koblick.com'),
-    'bcc':'groovyroovy@gmail.com',
+    'bcc': ['groovyroovy@gmail.com'],
+    'recipients': [ 'reuven@koblick.com'],
     'subject': 'Actions test',
-    'body': {'text': 'This is a {{ adj }} test.', 'context': {'adj': 'bad'}} }
+    'body': {'text': 'This is a {{ adj }} test.', 'context': {'adj': 'bad'}},
+    'sender': 'groovyroovy@koblick.com' }
 
-email_action = EmailAction('database', 'users', 'dirty', **msg)
+e_action = EmailAction('database', 'users', 'dirty', **msg)
 
 rows = db_session.query(User).all()
-email_action.load_params('users', 'first_name', 'id', rows)
-act_mgr.register_db_event(email_action)
+e_action.load_params('users', 'first_name', 'id', rows)
+act_mgr.register_db_event(e_action)
 
